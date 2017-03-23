fef6f30c78c92b5e2c5cc0fcf436584b14df705f
ivan996 <ivan.stanimirovic692@gmail.com>
2017-03-23 08:39:26 +0100

    implementiranje metode dodajKurs, brisanjeKursa,vratiKurs
    izmenjena metoda brisanjeKursa u interfejsu InterfejsMenjacnica

--- Menjacnica/src/interfejs/InterfejsMenjacnica.java
+++ Menjacnica/src/interfejs/InterfejsMenjacnica.java
@@ -6,6 +6,7 @@ import menjacnica.valute.Valute;
 
 public interface InterfejsMenjacnica {
 	public void dodajKurs(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv, String skracenica);
-	public void brisanjeKursa(String skracenica, GregorianCalendar dan);
+	public void brisanjeKursa(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv,
+			String skracenica);
 	public Valute vratiKurs(GregorianCalendar dan, String naziv);
 }
--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,30 +1,49 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.InterfejsMenjacnica;
 import menjacnica.valute.Valute;
 
-public class Menjacnica implements InterfejsMenjacnica{
+public class Menjacnica implements InterfejsMenjacnica {
+	LinkedList<Valute> valuta = new LinkedList<Valute>();
 
 	@Override
 	public void dodajKurs(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv,
 			String skracenica) {
-		
-		
+		Valute v = new Valute();
+		v.setDatumKursa(datum);
+		v.setDonjiKurs(donjiK);
+		v.setGornjiKurs(gornjiK);
+		v.setPunNaziv(naziv);
+		v.setSkracenica(skracenica);
+		v.setSrednjiKurs(srednjiK);
+		valuta.add(v);
+
 	}
 
 	@Override
-	public void brisanjeKursa(String skracenica, GregorianCalendar dan) {
-		
-		
+	public void brisanjeKursa(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv,
+			String skracenica) {
+		Valute v = new Valute();
+		v.setDatumKursa(datum);
+		v.setDonjiKurs(donjiK);
+		v.setGornjiKurs(gornjiK);
+		v.setPunNaziv(naziv);
+		v.setSkracenica(skracenica);
+		v.setSrednjiKurs(srednjiK);
+		valuta.remove(v);
 	}
 
 	@Override
 	public Valute vratiKurs(GregorianCalendar dan, String naziv) {
-		
+		for(int i=0;i<valuta.size();i++){
+			if(valuta.get(i).getPunNaziv().equals(naziv) && valuta.get(i).getDatumKursa().equals(dan)){
+				return valuta.get(i);
+			}
+		}
 		return null;
 	}
-	
 
 }
