a4999bb6c39ed02f06eb2e2357906acca6bd3cf7
ivan996 <ivan.stanimirovic692@gmail.com>
2017-03-23 08:29:46 +0100

    Izmenjena klasa Menjacnica
    dodato implementira interfejs InterfejsMenjacnica,ubacene metode iz
    interfejsa

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,5 +1,30 @@
 package menjacnica;
 
-public class Menjacnica {
+import java.util.GregorianCalendar;
+
+import interfejs.InterfejsMenjacnica;
+import menjacnica.valute.Valute;
+
+public class Menjacnica implements InterfejsMenjacnica{
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv,
+			String skracenica) {
+		
+		
+	}
+
+	@Override
+	public void brisanjeKursa(String skracenica, GregorianCalendar dan) {
+		
+		
+	}
+
+	@Override
+	public Valute vratiKurs(GregorianCalendar dan, String naziv) {
+		
+		return null;
+	}
+	
 
 }
