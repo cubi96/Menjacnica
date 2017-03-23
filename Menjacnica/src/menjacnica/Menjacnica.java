72ab9c7a2827292a27b705cb4cda53ef30d96bf9
ivan996 <ivan.stanimirovic692@gmail.com>
2017-03-23 08:45:10 +0100

    Implementirana metoda dodajKurs

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,16 +1,24 @@
 package menjacnica;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 import interfejs.InterfejsMenjacnica;
 import menjacnica.valute.Valute;
 
 public class Menjacnica implements InterfejsMenjacnica{
-
+	ArrayList<Valute> val = new ArrayList<Valute>();
 	@Override
 	public void dodajKurs(GregorianCalendar datum, double donjiK, double gornjiK, double srednjiK, String naziv,
 			String skracenica) {
-		
+		Valute v = new Valute();
+		v.setDatumKursa(datum);
+		v.setDonjiKurs(donjiK);
+		v.setGornjiKurs(gornjiK);
+		v.setPunNaziv(naziv);
+		v.setSkracenica(skracenica);
+		v.setSrednjiKurs(srednjiK);
+		val.add(v);
 		
 	}
 
